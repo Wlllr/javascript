df1aa379510862336bc9136f62987b4fa6ec4bc9
Weller <weller_pereira@hotmail.com>
2022-04-25 15:39:33 -0300

    brincando com JS

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,1 +1,11 @@
-alert("Hello World!");
+const firstName = "Weller";
+const lastName = "De Souza";
+
+let w;
+
+w = firstName.concat(" ", lastName);
+
+w = firstName.toUpperCase();
+w = lastName.toLower
+
+console.log(w)
