60392e1e2eeba10aff28071bcf771e125314ac39
Weller <weller_pereira@hotmail.com>
2022-05-09 13:42:28 -0300

    array exercise

---
+++ js_sandbox/array.js
@@ -0,0 +1,85 @@
+//ARRAYS
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+console.log(cars)
+
+console.log(cars[2])
+
+const houses = [];
+
+houses[0] = "433";
+houses[1] = "1618";
+houses[2] = "341";
+
+console.log(houses)
+
+const persons = new Array("Weller","Nathy","Yuri");
+
+console.log(persons);
+
+cars[0] = "Opel";
+
+console.log(cars)
+console.log(cars[0])
+
+console.log(typeof cars)
+
+console.log(persons[0])
+
+const date = {
+    now: "Quinta-feira"
+};
+
+function myFunction(){
+    return "hello";
+}
+
+const myArray = [];
+
+myArray[0] = date;
+myArray[1] = myFunction;
+myArray[2] = cars;
+
+console.log(myArray)
+
+console.log(myArray.sort())
+console.log(myArray)
+
+const fruits = ["Banana","Orange","Apple","Mango"];
+console.log(fruits.length)
+
+let fruit = fruits[0];
+console.log(fruit)
+
+let frui = fruits[fruits.length - 1] ;
+console.log(frui)
+
+
+let fLen = fruits.length;
+
+// for (let i = 0; i < fLen; i++){
+//     text += "<li>" + fruits[i] + "</li>";
+// }
+// text += "</ul>"
+
+// console.log(text)
+
+//for each
+
+// const numeros = [1,2,3,4,5];
+
+// let numm = "numero ";
+// numeros.forEach(numNum);
+// numm += "! "
+
+// function numNum(value){
+//     numm += value;
+// }
+
+// console.log(numm)
+
+// fruits.push("Lemon");
+
+// console.log(fruits)
+
